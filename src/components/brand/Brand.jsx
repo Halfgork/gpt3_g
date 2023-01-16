5d55480d65f27437d8b045b1c4396405b0586803
Halfgork <gorkemsakalak@gmail.com>
2023-01-16 20:09:52 +0300

    update

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { google, slack, atlassian, dropbox, shopify } from './import';
+import { google, slack, atlassian, dropbox, shopify } from './imports';
 import './brand.css';
 
 const Brand = () => (
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Article from '../../components/article/Article';
-import { blog01, blog02, blog03, blog04, blog05 } from './import';
+import { blog01, blog02, blog03, blog04, blog05 } from './imports';
 import './blog.css';
 
 const Blog = () => (
